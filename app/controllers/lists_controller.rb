c1ffc3d4f0a94383b99c5dc6ac15a1cb1ed8c94d
Nicole Deutrich <nicoledeutrich@t-online.de>
2023-08-17 15:37:57 +0200

    add Lists controller actions index, show and new

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,25 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(:id)
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
