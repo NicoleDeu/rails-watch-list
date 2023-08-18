ef89e56da400cbd9e63723ac30693f96142a3157
Nicole Deutrich <nicoledeutrich@t-online.de>
2023-08-18 12:40:54 +0200

    update gem file lock

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: %i[index show new create]
+  resources :bookmarks, only: %i[new create destroy]
 end
